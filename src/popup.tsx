cb128ff6e5bb0005f4852ef94e131c8f982466cc
Deepak Bhattarai <deepakbhattarai289@gmail.com>
2024-05-21 09:25:09 +0545

    Add popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,26 @@
 import "@/style.css";
 
 function IndexPopup() {
-  return <div className="flex items-center justify-center h-16 w-40"></div>;
+  return (
+    <div className="w-72 rounded-lg font-serif">
+      <div className="p-4 max-w-md mx-auto">
+        <h1 className="text-2xl font-bold mb-4 text-center">Website Preview</h1>
+        <article className="text-sm text-gray-700">
+          <h2 className="text-lg font-semibold mb-2">About this Extension</h2>
+          <p className="mb-4">
+            The Website Preview extension allows you to quickly and easily
+            preview websites directly from your browser.{" "}
+          </p>
+          <h3 className="text-md font-medium mb-2">Key Features</h3>
+          <ul className="list-disc list-inside mb-4">
+            <li>Quick previews of any website</li>
+            <li>Seamless integration with your browser</li>
+            <li>User-friendly interface</li>
+          </ul>
+        </article>
+      </div>
+    </div>
+  );
 }
 
 export default IndexPopup;
